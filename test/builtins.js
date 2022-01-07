f8d0107be992c6fef30acf7f61b3b4c4d69f5882
Marcello Del Buono <m.delbuono@gmail.com>
2022-01-07 23:08:04 +0100

    Modified List Builtins

--- test/builtins.js
+++ test/builtins.js
@@ -896,167 +896,199 @@ describe("builtins", () => {
         });              
     });
     
-    // describe("List", () => {
-    // 
-    //     describe("List.size(array)", () => {
-    // 
-    //         it("should return the array length", () => {
-    //             expect(List.size([1,2,3])).to.equal(3);
-    //             expect(List.size([])).to.equal(0);
-    //         });
-    // 
-    //         it("shoudl throw a type error if the passed argument is not a string", () => {
-    //             expect(() => List.size({})).to.throw(TypeError);
-    //         });
-    //     });
-    // 
-    //     describe("List.map(fn)(items)", () => {
-    // 
-    //         it("should return an array containing the image of the items through fn", async () => {
-    //             const fn = x => 2*x;
-    //             expect(await List.map(fn)([1,2,3])).to.deep.equal([2,4,6]);
-    //         });
-    // 
-    //         it("should throw an error if the passed parameter is not an array", async () => {
-    //             try {
-    //                 await List.map(x=>x)({});
-    //                 throw new Error("It did not throw!");
-    //             } catch (e) {
-    //                 expect(e).to.be.instanceof(TypeError);
-    //                 expect(e.message).to.equal("List type expected");
-    //             }
-    //         });
-    //     });
-    // 
-    //     describe("List.find: Item -> List -> Numb", () => {
-    // 
-    //         it("should return the first index of Item in List", () => {
-    //             expect(List.find(20)).to.be.a("function");
-    //             expect(List.find(20)([0,10,20,10,20])).to.equal(2);
-    //         });
-    // 
-    //         it("should return -1 if no match is found", () => {
-    //             expect(List.find(50)).to.be.a("function");
-    //             expect(List.find(50)([0,10,20,10,20])).to.equal(-1);
-    //         });
-    // 
-    //         it("should return throw an error is List is not a list", () => {
-    //             const fn = List.find(50);
-    //             expect(fn).to.be.a("function");
-    //             expect(() => fn("abc")).to.throw(TypeError);
-    //         });            
-    //     });
-    // 
-    //     describe("List.rfind: Item -> List -> Numb", () => {
-    // 
-    //         it("should return the first index of Item in List", () => {
-    //             expect(List.rfind(20)).to.be.a("function");
-    //             expect(List.rfind(20)([0,10,20,10,20])).to.equal(4);
-    //         });
-    // 
-    //         it("should return -1 if no match is found", () => {
-    //             expect(List.rfind(50)).to.be.a("function");
-    //             expect(List.rfind(50)([0,10,20,10,20])).to.equal(-1);
-    //         });
-    // 
-    //         it("should return throw an error is List is not a list", () => {
-    //             const fn = List.rfind(50);
-    //             expect(fn).to.be.a("function");
-    //             expect(() => fn("abc")).to.throw(TypeError);
-    //         });            
-    //     });
-    // 
-    //     describe("List.join: (sep:Text) -> (list:List of Text) -> Text", () => {
-    // 
-    //         it("should return a string obtaining by concatenating the list item with interposed separator", () => {
-    //             const fn = List.join("-");
-    //             expect(fn).to.be.a("function");
-    //             expect(fn(["a","b","c"])).to.equal("a-b-c");
-    //             expect(fn([])).to.equal("");
-    //         });
-    // 
-    //         it("should throw an erro if the separator is not a string", () => {
-    //             expect(() => List.join(1)).to.throw(TypeError);
-    //         });
-    // 
-    //         it("should throw an error if list is not a List", () => {
-    //             const fn = List.join("-");
-    //             expect(fn).to.be.a("function");
-    //             expect(() => fn("abc")).to.throw(TypeError);
-    //         });
-    // 
-    //         it("should throw an error if any of the list items is not a string", () => {
-    //             const fn = List.join("-");
-    //             expect(fn).to.be.a("function");
-    //             expect(() => fn(["abc", 1, "def"])).to.throw(TypeError);
-    //         });
-    //     });        
-    // 
-    //     describe("List.reverse: List -> List", () => {
-    // 
-    //         it("should return a copy of the passed list, in reversed order", async () => {
-    //             var l1 = [1,2,3,4,5];
-    //             var l2 = List.reverse(l1);
-    //             expect(l1).to.not.equal(l2);
-    //             expect(l2).to.deep.equal([5,4,3,2,1]);
-    //         });
-    // 
-    //         it("should throw an error if the passed item is not a List", () => {
-    //             expect(() => List.reverse("abc")).to.throw(TypeError);
-    //         });
-    //     });
-    // 
-    //     describe("List.head: (i:Numb) -> (l:List) -> List", () => {
-    // 
-    //         it("should return the sublist of the first i items of l", () => {
-    //             const fn = List.head(3);
-    //             expect(fn).to.be.a("function");
-    //             expect(fn([1,2,3,4,5,6])).to.deep.equal([1,2,3]);
-    //         });
-    // 
-    //         it("should consider relative inices as relative to the end of the list", () => {
-    //             const fn = List.head(-3);
-    //             expect(fn).to.be.a("function");
-    //             expect(fn([1,2,3,4,5,6,7])).to.deep.equal([1,2,3,4]);
-    //         });
-    // 
-    //         it("should throw an error if i is not a number", () => {
-    //             expect(() => List.head("abc")).to.throw(TypeError);
-    //         });
-    // 
-    //         it("should throw an error if l is not a list", () => {
-    //             const fn = List.head(3);
-    //             expect(fn).to.be.a("function");
-    //             expect(() => fn("abc")).to.throw(TypeError);
-    //         });
-    //     });              
-    // 
-    //     describe("List.tail: (i:Numb) -> (l:List) -> List", () => {
-    // 
-    //         it("should return the sublist of the items of l afther and included the i-th item", () => {
-    //             const fn = List.tail(3);
-    //             expect(fn).to.be.a("function");
-    //             expect(fn([1,2,3,4,5,6,7])).to.deep.equal([4,5,6,7]);
-    //         });
-    // 
-    //         it("should consider relative inices as relative to the end of the list", () => {
-    //             const fn = List.tail(-3);
-    //             expect(fn).to.be.a("function");
-    //             expect(fn([1,2,3,4,5,6,7])).to.deep.equal([5,6,7]);
-    //         });
-    // 
-    //         it("should throw an error if i is not a number", () => {
-    //             expect(() => List.tail("abc")).to.throw(TypeError);
-    //         });
-    // 
-    //         it("should throw an error if l is not a list", () => {
-    //             const fn = List.tail(3);
-    //             expect(fn).to.be.a("function");
-    //             expect(() => fn("abc")).to.throw(TypeError);
-    //         });
-    //     });              
-    // });
-    // 
+    describe("List", () => {
+    
+        describe.skip("List.size(array)", () => {
+    
+            it("should return the array length", () => {
+                expect(List.size([1,2,3])).to.equal(3);
+                expect(List.size([])).to.equal(0);
+            });
+    
+            it("shoudl throw a type error if the passed argument is not a string", () => {
+                expect(() => List.size({})).to.throw(TypeError);
+            });
+        });
+    
+        describe("l2 = List.reverse l1", () => {
+    
+            it("should return l1 in reversed order", async () => {
+                expect(await evaluate("List.reverse [1,2,3]")).to.be.List([3,2,1]);
+            });
+            
+            it("should return Undefined List if the l1 is not a List", async () => {
+                expect(await evaluate("List.reverse 123")).to.be.Undefined("List");
+            });
+
+            it("should apply to all the items and return a tuple if the argument is a truple", async () => {
+                expect(await evaluate("List.reverse([1,2,3],[4,5,6])")).to.be.Tuple([[3,2,1],[6,5,4]]);
+            });                        
+        });
+    
+        describe("f = List.find x", () => {
+            
+            describe("when x is an item", () => {
+                
+                it("should return a function", async () => {
+                    expect(await evaluate("List.find 10")).to.be.instanceof(types.Func);
+                });
+
+                describe("i = f L ", () => {
+        
+                    it("should return the first index of x in L", async () => {
+                        expect(await evaluate("List.find 'Abc' ['Abc', 'def', 'Abc']")).to.be.Numb(0);
+                        expect(await evaluate("List.find 'Abc' ['abc', 'def', 'Abc']")).to.be.Numb(2);
+                    });
+        
+                    it("should return -1 if no match is found", async () => {
+                        expect(await evaluate("List.find 'xxx' ['Abc', 'def', 'Abc']")).to.be.Numb(-1);
+                    });
+        
+                    it.skip("should return Undefined Text if `s1` is not a string", async () => {
+                        expect(await evaluate("Text.find 10")).to.be.instanceof(types.Func);
+                        expect(await evaluate("Text.find 10 'abc'")).to.be.Undefined("Text");
+                    });
+
+                    it.skip("should return Undefined Text if `s2` is not a string", async () => {
+                        expect(await evaluate("Text.find 'abc' 10")).to.be.Undefined("Text");
+                    });
+
+                    it.skip("should return a tuple of values if `s2` is a tuple", async () => {
+                        expect(await evaluate("Text.find 'Abc' ('__Abc__def__Abc', '01234Abc__')")).to.be.Tuple([2,5]);
+                    });
+                });
+            });
+
+            describe("when the argument is a tuple", () => {
+                
+                it.skip("should return one `find` function for each item of the argument", async () => {
+                    const F = await evaluate("Text.find('Abc','Def')");
+                    expect(F).to.be.instanceof(types.Tuple);
+
+                    const [f1,f2] = Array.from(F.items());
+                    expect(f1).to.be.instanceof(types.Func)
+                    expect(f2).to.be.instanceof(types.Func)
+                    
+                    expect(await evaluate("Text.find('Abc','Def') '__Abc__def__Def'")).to.be.Tuple([2,12]);
+                    expect(await evaluate("Text.find('Abc','Def')('__Abc__Def__Abc', '01234Abc__')")).to.be.Tuple([2,5,7,-1]);                    
+                });
+            });    
+        });    
+    
+        describe.skip("List.rfind: Item -> List -> Numb", () => {
+    
+            it("should return the first index of Item in List", () => {
+                expect(List.rfind(20)).to.be.a("function");
+                expect(List.rfind(20)([0,10,20,10,20])).to.equal(4);
+            });
+    
+            it("should return -1 if no match is found", () => {
+                expect(List.rfind(50)).to.be.a("function");
+                expect(List.rfind(50)([0,10,20,10,20])).to.equal(-1);
+            });
+    
+            it("should return throw an error is List is not a list", () => {
+                const fn = List.rfind(50);
+                expect(fn).to.be.a("function");
+                expect(() => fn("abc")).to.throw(TypeError);
+            });            
+        });
+    
+        describe.skip("List.join: (sep:Text) -> (list:List of Text) -> Text", () => {
+    
+            it("should return a string obtaining by concatenating the list item with interposed separator", () => {
+                const fn = List.join("-");
+                expect(fn).to.be.a("function");
+                expect(fn(["a","b","c"])).to.equal("a-b-c");
+                expect(fn([])).to.equal("");
+            });
+    
+            it("should throw an erro if the separator is not a string", () => {
+                expect(() => List.join(1)).to.throw(TypeError);
+            });
+    
+            it("should throw an error if list is not a List", () => {
+                const fn = List.join("-");
+                expect(fn).to.be.a("function");
+                expect(() => fn("abc")).to.throw(TypeError);
+            });
+    
+            it("should throw an error if any of the list items is not a string", () => {
+                const fn = List.join("-");
+                expect(fn).to.be.a("function");
+                expect(() => fn(["abc", 1, "def"])).to.throw(TypeError);
+            });
+        });        
+    
+        describe.skip("List.head: (i:Numb) -> (l:List) -> List", () => {
+    
+            it("should return the sublist of the first i items of l", () => {
+                const fn = List.head(3);
+                expect(fn).to.be.a("function");
+                expect(fn([1,2,3,4,5,6])).to.deep.equal([1,2,3]);
+            });
+    
+            it("should consider relative inices as relative to the end of the list", () => {
+                const fn = List.head(-3);
+                expect(fn).to.be.a("function");
+                expect(fn([1,2,3,4,5,6,7])).to.deep.equal([1,2,3,4]);
+            });
+    
+            it("should throw an error if i is not a number", () => {
+                expect(() => List.head("abc")).to.throw(TypeError);
+            });
+    
+            it("should throw an error if l is not a list", () => {
+                const fn = List.head(3);
+                expect(fn).to.be.a("function");
+                expect(() => fn("abc")).to.throw(TypeError);
+            });
+        });              
+    
+        describe.skip("List.tail: (i:Numb) -> (l:List) -> List", () => {
+    
+            it("should return the sublist of the items of l afther and included the i-th item", () => {
+                const fn = List.tail(3);
+                expect(fn).to.be.a("function");
+                expect(fn([1,2,3,4,5,6,7])).to.deep.equal([4,5,6,7]);
+            });
+    
+            it("should consider relative inices as relative to the end of the list", () => {
+                const fn = List.tail(-3);
+                expect(fn).to.be.a("function");
+                expect(fn([1,2,3,4,5,6,7])).to.deep.equal([5,6,7]);
+            });
+    
+            it("should throw an error if i is not a number", () => {
+                expect(() => List.tail("abc")).to.throw(TypeError);
+            });
+    
+            it("should throw an error if l is not a list", () => {
+                const fn = List.tail(3);
+                expect(fn).to.be.a("function");
+                expect(() => fn("abc")).to.throw(TypeError);
+            });
+        });              
+        
+        describe.skip("List.map(fn)(items)", () => {
+    
+            it("should return an array containing the image of the items through fn", async () => {
+                const fn = x => 2*x;
+                expect(await List.map(fn)([1,2,3])).to.deep.equal([2,4,6]);
+            });
+    
+            it("should throw an error if the passed parameter is not an array", async () => {
+                try {
+                    await List.map(x=>x)({});
+                    throw new Error("It did not throw!");
+                } catch (e) {
+                    expect(e).to.be.instanceof(TypeError);
+                    expect(e.message).to.equal("List type expected");
+                }
+            });
+        });
+    });
+    
     // describe("Namespace", () => {
     // 
     //     describe("Namespace.size(object)", () => {
