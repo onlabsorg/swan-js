eddab11007fdd2e942aa814adc21f2116eded8c2
Marcello Del Buono <m.delbuono@gmail.com>
2022-05-06 19:54:23 +0200

    modules/debug: added inspect function

--- docs/modules/debug.md
+++ docs/modules/debug.md
@@ -6,4 +6,8 @@ debug.log: Term t -> Text id
 ------------------------------------------------------------------------
 The log function writes the passed item to the javascript console.
   
+debug.inspect: Term t -> Namespace info
+------------------------------------------------------------------------
+Returns an object containing detailed information about the passed term.
+  
 
--- test/modules/debug.js
+++ test/modules/debug.js
@@ -41,5 +41,140 @@ describe("debug module", function () {
             console.log = jslog;
         });
     });
+    
+    describe("debug.inspect", () => {
+        
+        describe("when the argument is a Bool", () => {
+            
+            it("should return an object with type and value", async () => {
+                expect(await evaluate("debug.inspect(1==1)")).to.be.Namespace({
+                    type: "Bool",
+                    value: true
+                })
+            });
+        });
+        
+        describe("when the argument is a Numb", () => {
+            
+            it("should return an object with type and value", async () => {
+                expect(await evaluate("debug.inspect 123")).to.be.Namespace({
+                    type: "Numb",
+                    value: 123
+                })
+            });            
+        });
+        
+        describe("when the argument is a Text", () => {
+            
+            it("should return an object with type and value", async () => {
+                expect(await evaluate("debug.inspect 'abc'")).to.be.Namespace({
+                    type: "Text",
+                    value: "abc"
+                })
+            });            
+        });
+        
+        describe("when the argument is a List", () => {
+
+            it("should return an object with type and an array of inspection info as value", async () => {
+                expect(await evaluate("debug.inspect [1, 'abc', 2]")).to.be.Namespace({
+                    type: "List",
+                    value: [
+                        {type:"Numb", value:1},
+                        {type:"Text", value:"abc"},
+                        {type:"Numb", value:2}
+                    ]
+                })
+            });                        
+        });
+        
+        describe("when the argument is a Namespace", () => {
+            
+            it("should return an object with type and an mapping of inspection info as value", async () => {
+                expect(await evaluate("debug.inspect {a:1, b:2, c:3}")).to.be.Namespace({
+                    type: "Namespace",
+                    value: {
+                        a: {type:"Numb", value:1},
+                        b: {type:"Numb", value:2},
+                        c: {type:"Numb", value:3}
+                    }
+                })
+            });                                    
+        });
+        
+        describe("when the argument is a Func", () => {
+            
+            it("should return an object with type='Func'", async () => {
+                expect(await evaluate("debug.inspect(x->x)")).to.be.Namespace({
+                    type: "Func",
+                })
+            });                                    
+        });
+        
+        describe("when the argument is a Tuple", () => {
+            
+            it("should return an object with type and an array of inspection info as value", async () => {
+                expect(await evaluate("debug.inspect(1, 2, 3)")).to.be.Namespace({
+                    type: "Tuple",
+                    value: [
+                        {type:"Numb", value:1},
+                        {type:"Numb", value:2},
+                        {type:"Numb", value:3}
+                    ]
+                })
+            });                        
+
+            it("should return an the first item inspection info if it is made of one item only", async () => {
+                expect(await evaluate("debug.inspect(10, ())")).to.be.Namespace({
+                    type: "Numb",
+                    value: 10
+                })
+            });                        
+
+            it("should return the object {type:'Nothing'} if it is an empty tuple", async () => {
+                expect(await evaluate("debug.inspect()")).to.be.Namespace({
+                    type: "Nothing"
+                })
+            });                        
+        });
+        
+        describe("when the argument is an Undefined term", () => {
+            
+            it("should return the info object {Text type, Text operation, List arguments}", async () => {
+                const presets = {undefined: (type, ...args) => new types.Undefined(type, ...args)};
+                expect(await evaluate("debug.inspect(undefined('DummyOp', 1, 2))", presets)).to.be.Namespace({
+                    type: "Undefined",
+                    operation: "DummyOp",
+                    arguments: [
+                        {type:"Numb", value:1},
+                        {type:"Numb", value:2},
+                    ]
+                })
+            });            
+
+            it("should return the info object containing also {Text source, Numb position} if the Undefined item contains a position", async () => {
+                expect(await evaluate(`debug.inspect(
+                        unknown_func())
+                        `)).to.be.Namespace({
+                    type: "Undefined",
+                    operation: "ApplyOperation",
+                    arguments: [
+                        {
+                            type: "Undefined",
+                            operation: "NameReference",
+                            arguments: [
+                                {type:'Text', value:"unknown_func"}
+                            ],
+                            source: "                        unknown_func())",
+                            position: 36
+                        },
+                        {type:"Nothing"}
+                    ],
+                    source: "                        unknown_func())",
+                    position: 36
+                })
+            });            
+        });
+    });
 });
 
